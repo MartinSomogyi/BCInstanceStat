2d97d2ca0b306fb8d128723d612f56ccb567c4fe
Martin Somogyi <Martin.Somogyi@sci-net.co.uk>
2025-10-28 15:01:33 +0000

    Import BC module

--- stats1.ps1
+++ stats1.ps1
@@ -1,2 +1,4 @@
 #Testing to see how result is returned on remotely invoked scripts
+Import-Module "C:\Program Files\Microsoft Dynamics 365 Business Central\270\Service\NavAdminTool.ps1"
+
 (Get-NAVServerConfiguration -ServerInstance Bc_Dev) | Out-GridView
